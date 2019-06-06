3d8e2f54bccdf46f299de2e2d34b166d50974b5e
Campbell <mikecampbell@quickenloans.com>
2019-06-05 20:14:06 -0400

    Added border logic for selected color

--- script.js
+++ script.js
@@ -2,23 +2,31 @@ let selectedColor;
 let currentColor = "blank";
 
 let redBtn = document.getElementById("redButton");
-console.log(redBtn);
 let greenBtn = document.getElementById("greenButton");
 let blueBtn = document.getElementById("blueButton");
 
 
 redBtn.addEventListener("click", function(){
     selectedColor = "red";
+    redBtn.style.borderColor = ("white");
+    blueBtn.style.borderColor = ("black");
+    greenBtn.style.borderColor = ("black");
     console.log(selectedColor);
 });
 
 greenBtn.addEventListener("click", function(){
     selectedColor = "green";
+    redBtn.style.borderColor = ("black");
+    blueBtn.style.borderColor = ("black");
+    greenBtn.style.borderColor = ("white");
     console.log(selectedColor);
 });
 
 blueBtn.addEventListener("click", function(){
     selectedColor = "blue";
+    redBtn.style.borderColor = ("black");
+    blueBtn.style.borderColor = ("white");
+    greenBtn.style.borderColor = ("black");
     console.log(selectedColor);
 });
 
