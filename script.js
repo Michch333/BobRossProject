2a35620347a251a293069ad0c6550a8c3dbfc3fa
Lenny Hughes <lenaudhughes@gmail.com>
2019-06-05 21:05:18 -0400

    refactored grid parent eventlistener

--- script.js
+++ script.js
@@ -24,35 +24,8 @@ blueBtn.addEventListener("click", function(){
     console.log(selectedColor);
 });
 
-
-gridParent.addEventListener("click", function(e){
-   if(e.target.matches('.a1')){
+gridParent.addEventListener("click",function(e){
+    if(e.target.matches('.gridBlocks')){
         e.target.style.backgroundColor = "Red";
-   }
-   if(e.target.matches('.a2')){
-       e.target.style.backgroundColor = "Blue";
-   }
-   if(e.target.matches('.a3')){
-       e.target.style.backgroundColor = "light blue";
-   }
-   if(e.target.matches('.b1')){
-       e.target.style.backgroundColor = "light blue";
-   }
-   if(e.target.matches('.b2')){
-       e.target.style.backgroundColor = "light blue";
-   }
-   if(e.target.matches('.b3')){
-       e.target.style.backgroundColor = "light blue";
-   }
-   if(e.target.matches('.c1')){
-       e.target.style.backgroundColor = "light blue";
-   }
-   if(e.target.matches('.c2')){
-       e.target.style.backgroundColor = "light blue";
-   }
-   if(e.target.matches('.c3')){
-       e.target.style.backgroundColor = "light blue";
-   }
-
-
+    }
 });
